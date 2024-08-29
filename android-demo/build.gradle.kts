fabef92cec4aaec50fb4690ff7c9dd59ab3f3a07
nmssalman <nmssalman@outlook.com>
2024-08-29 12:10:18 +0530

    2.0

--- android-demo/build.gradle.kts
+++ android-demo/build.gradle.kts
@@ -1,8 +1,18 @@
+import org.jetbrains.kotlin.load.java.setMethodName
+
 plugins {
     alias(libs.plugins.android.library)
+    id("maven-publish")
 }
-
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        mavenLocal()
+    }
+}
 android {
+
     namespace = "com.nmssalman.android_demo"
     compileSdk = 34
 
@@ -20,13 +30,49 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            consumerProguardFiles("proguard-rules.pro")
+            consumerProguardFiles("consumer-rules.pro")
         }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+
+
+
+
 }
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)        // << --- ADD This
+    }
+}
+
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17            // << --- ADD This
+    targetCompatibility = JavaVersion.VERSION_17
+}
+publishing {
+    repositories {               // << --- ADD This
+        mavenLocal()
+    }
+    publications {
+        create<MavenPublication>("maven")
+        {
+            groupId = "com.github.nmssalman"
+            artifactId = "jitpack_example"
+            version = "1.0"
+            pom {
+                description = "DESCRIPTION"
+            }
+        }
+    }
+
+}
+
 
 dependencies {
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,10 +7,12 @@ pluginManagement {
                 includeGroupByRegex("androidx.*")
             }
         }
+        mavenLocal()
         mavenCentral()
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
