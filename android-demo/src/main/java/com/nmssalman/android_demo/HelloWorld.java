5e624898335d4366a94913f3b11278b64b1bedea
nmssalman <nmssalman@outlook.com>
2024-08-29 11:09:38 +0530

    init

---
+++ android-demo/src/main/java/com/nmssalman/android_demo/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.nmssalman.android_demo;
+
+import android.util.Log;
+
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+
+    }
+
+    public static void printLog(String tag, String message){
+        Log.d(tag, message);
+    }
+}
---
+++ app/src/main/java/com/nmssalman/examplemaven/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.nmssalman.examplemaven
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.nmssalman.android_demo.HelloWorld
+import com.nmssalman.examplemaven.ui.theme.ExampleMavenTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        HelloWorld.printLog("MavenAndroidDemo", "HELLO WORLD")
+    }
+}
