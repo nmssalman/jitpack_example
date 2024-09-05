ad8d4e90c89e5862706a638a05311945a6713cf3
nmssalman <nmssalman@outlook.com>
2024-09-05 14:15:31 +0530

    fixes in bubbleshowcase

--- android-demo/src/main/java/com/nmssalman/android_demo/HelloWorld.java
+++ android-demo/src/main/java/com/nmssalman/android_demo/HelloWorld.java
@@ -5,6 +5,7 @@ import android.util.Log;
 public class HelloWorld {
     private PageIndicatorView pageIndicatorView;
 
+
     public void TestingFunction(){
         Log.d("TestingFunction", String.valueOf(System.currentTimeMillis()));
     }
