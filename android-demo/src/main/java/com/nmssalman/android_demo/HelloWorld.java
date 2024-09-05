c46b89bb83feba42458e4f6e69a87d39a52393c0
nmssalman <nmssalman@outlook.com>
2024-09-05 12:35:09 +0530

    fixes

--- android-demo/src/main/java/com/nmssalman/android_demo/HelloWorld.java
+++ android-demo/src/main/java/com/nmssalman/android_demo/HelloWorld.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 public class HelloWorld {
     private PageIndicatorView pageIndicatorView;
+
     public void TestingFunction(){
         Log.d("TestingFunction", String.valueOf(System.currentTimeMillis()));
     }
